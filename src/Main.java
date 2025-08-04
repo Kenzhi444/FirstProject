69b5486dab19bac7e5ff88871ee56f9cd331dde5
stas95k <stas95k@mail.ru>
2025-08-04 21:31:38 +0300

    Первое приближение без смены игрока

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int playerOne = 1;
-        int pleyerTwo = 2;
 
         char[][] gameGrid = new char[3][3];
         for (int i = 0; i < 3; i++) {
@@ -13,25 +11,40 @@ public class Main {
                 gameGrid[i][j] = ' ';
             }
         }
-
-        Scanner console = new Scanner(System.in);
-        int line = console.nextInt();
-        int column = console.nextInt();
-        boolean validCell;
-        if (gameGrid[line][column] == ' ') {
-            validCell = true;
-        } else {
-            validCell = false;
-        }
-        if (validCell) {
-            gameGrid[line][column] = 'X';
-            if (gameGrid[line + 1][column] == 'X' && gameGrid[line - 1][column] == 'X') {
-                System.out.println("Победа!!!");
-            } else if (gameGrid[line][column + 1] == 'X' && gameGrid[line][column - 1] == 'X') {
-                System.out.println("Победа!!!");
+        boolean victory = false;
+        while (!victory) {
+            Scanner console = new Scanner(System.in);
+            int line = console.nextInt();
+            int column = console.nextInt();
+            boolean validCell;
+            if (gameGrid[line][column] == ' ') {
+                validCell = true;
+            } else {
+                validCell = false;
             }
-        } else {
-            System.out.println("Ячейка занята, выберите другую!");
+            if (validCell) {
+                gameGrid[line][column] = 'X';
+                if (gameGrid[line + 1][column] == 'X' && gameGrid[line - 1][column] == 'X') {
+                    victory = true;
+                    System.out.println("Победа!!!");
+                } else if (gameGrid[line][column + 1] == 'X' && gameGrid[line][column - 1] == 'X') {
+                    victory = true;
+                    System.out.println("Победа!!!");
+                }
+            } else {
+                System.out.println("Ячейка занята, выберите другую!");
+            }
+            // Проверка на ничью
+            int count = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (gameGrid[i][j] == ' ') {
+                        count++;
+                        break;
+                    }
+                }
+                System.out.println("Ничья!!!");
+            }
         }
     }
 }
